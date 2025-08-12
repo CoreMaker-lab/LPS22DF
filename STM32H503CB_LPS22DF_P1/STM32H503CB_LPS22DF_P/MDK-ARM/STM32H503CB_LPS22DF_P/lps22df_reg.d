a0216ab7b0825d40fce42e3ac553d0f5a034294d
CoreMaker <a845656974@outlook.com>
2025-08-12 23:40:38 +0800

    update

---
+++ STM32H503CB_LPS22DF_P1/STM32H503CB_LPS22DF_P/MDK-ARM/STM32H503CB_LPS22DF_P/lps22df_reg.d
@@ -0,0 +1,5 @@
+stm32h503cb_lps22df_p/lps22df_reg.o: ..\Core\Src\lps22df_reg.c \
+  ..\Core\Inc\lps22df_reg.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h
